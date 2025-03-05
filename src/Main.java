7578274db18650d38e272362a1ee9040d42fff55
kibeo <kibeom0806@gmail.com>
2025-03-05 15:25:29 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        InputProcessor iProcessor = new InputProcessor();
-        Calculator calculator = new Calculator();
-        Log log = Log.getInstance();
-        OutputProcessor oProcessor = new OutputProcessor();
-        List<Token> tokens = new ArrayList<Token>();
-        Printer printer = new Printer();
         CalFrame calFrame = CalFrame.getInstance();
         double result=0;
 
-//        while(true){
-//            tokens = iProcessor.init();
-//            result = calculator.init(tokens);
-//            oProcessor.init(result, tokens);
-//
-//            tokens.clear();
-//        }
     }
 }
